333fb6433525fd8041631e993ee415d86f3091fa
Hoyong Jeong <ghdyd586@gmail.com>
2023-06-05 15:46:39 +0900

    Uploaded Final FIles

---
+++ gate.h
@@ -0,0 +1,2 @@
+void make_gate(int selmap,  vector<snakebody>& gate, int* cnt2);
+void cross_gate(int out_gate_row, int out_gate_col, int* new_head_row, int* new_head_col, int* snake_direction, int selmap);
